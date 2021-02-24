8eb1c26a14cab69d49018ce89669c19397359c1d
Daniel Serrano <daniel.serrano@koombea.com>
2021-02-24 11:43:33 -0500

    - added test case to contact_importer_spec.rb where an object might be invalid and create an importer log

--- spec/services/imports/contact_importer_spec.rb
+++ spec/services/imports/contact_importer_spec.rb
@@ -4,13 +4,20 @@ require 'rails_helper'
 
 RSpec.describe Imports::ContactImporter do
   describe '#call' do
-    subject { Imports::ContactImporter.new(contacts_file: build(:contacts_file))}
-    context 'when the contact only has valid objects' do
+    subject { described_class.new(contacts_file: build(:contacts_file)) }
+    context 'when the contacts file only has valid objects' do
       it 'creates contact objects' do
         subject.call
         expect(subject.contacts_file.user.contacts.size).to eq(1)
       end
+    end
 
+    context 'when the contacts file has invalid objects' do
+      subject { described_class.new(contacts_file: build(:contacts_file, :contacts_file_with_errors)) }
+      it 'should create importer log object' do
+        subject.call
+        expect(subject.contacts_file.importer_logs.size).to eq(1)
+      end
     end
 
     context 'when header mappings are not set' do
