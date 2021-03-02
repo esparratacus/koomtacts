dc223e8f8499dd8e1b5e97620fee0021e29b9a7e
Daniel Serrano <daniel.serrano@koombea.com>
2021-03-02 15:48:44 -0500

    - JWT token decoding - JWT token decoding test

--- app/services/api_authorization/token_manager.rb
+++ app/services/api_authorization/token_manager.rb
@@ -2,10 +2,20 @@ module ApiAuthorization
   class TokenManager
     class TokenEncodingError < StandardError; end
 
+    class TokenDecodingError < StandardError; end
+
+    KEY = Rails.application.secret_key_base
+
     def self.encode_token(payload)
       raise TokenEncodingError if payload.nil?
 
-      JWT.encode(payload, Rails.application.secret_key_base)
+      JWT.encode(payload, KEY)
+    end
+
+    def self.decoded_token(token)
+      JWT.decode(token, KEY, true, algorithm: 'HS256')[0]
+    rescue JWT::DecodeError
+      raise TokenDecodingError
     end
   end
 end
--- spec/services/api_authorization/token_manager_spec.rb
+++ spec/services/api_authorization/token_manager_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 
 RSpec.describe ApiAuthorization::TokenManager do
   let(:user) { create :user }
+  let(:user_data) { { 'user_id' => user.id } }
   subject { described_class }
   describe '#encode_token' do
     context 'when a hash is provided to the method' do
@@ -14,6 +15,26 @@ RSpec.describe ApiAuthorization::TokenManager do
     context 'when nothing is provided to the method' do
       it 'raises a TokenEncodingError exception' do
         expect{ subject.encode_token(nil) }.to raise_error(subject::TokenEncodingError)
+      end
+    end
+  end
+  describe '#decoded_token' do
+    let(:token) { subject.encode_token({ user_id: user.id }) }
+    context 'successfully decodes token into a user hash' do
+      it 'return user data hash' do
+        expect(subject.decoded_token(token)).to(eq(user_data))
+      end
+    end
+
+    context 'wrong token passed to method' do
+      it 'raises a TokenDecodingError' do
+        expect{ subject.decoded_token('invalidtoken') }.to raise_error(subject::TokenDecodingError)
+      end
+    end
+
+    context 'no token provided to method' do
+      it 'raises a TokenDecodingError' do
+        expect{ subject.decoded_token(nil) }.to raise_error(subject::TokenDecodingError)
       end
     end
   end
