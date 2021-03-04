7ad7dacdb911709452aaf7617b6063fe1aca7672
Daniel Serrano <daniel.serrano@koombea.com>
2021-03-04 11:21:05 -0500

    - create user action for api - create user tests for api

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,9 +1,38 @@
 module Api::V1
   class ApiController < ActionController::API
+    attr_reader :current_user
+
     include JsonapiErrorsHandler
     ErrorMapper.map_errors!({
                               'ActiveRecord::RecordNotFound' => 'JsonapiErrorsHandler::Errors::NotFound'
                             })
     rescue_from ::StandardError, with: lambda { |e| handle_error(e) }
+
+    rescue_from ActiveRecord::RecordInvalid, with: lambda { |e| handle_validation_error(e) }
+    rescue_from ActiveModel::ValidationError, with: lambda { |e| handle_validation_error(e) }
+
+    def handle_validation_error(error)
+      error_model = error.try(:model) || error.try(:record)
+      mapped = JsonapiErrorsHandler::Errors::Invalid.new(errors: error_model.errors)
+      render_error(mapped)
+    end
+
+    def auth_header
+      request.headers['Authorization']
+    end
+    def logged_in_user
+      if auth_header
+        decoded_token = JWT.decoded_token(auth_header.split(' ')[1])
+        @curent_user = User.find_by(decoded_token)
+      end
+    end
+
+    def is_logged_in?
+      !!logged_in_user
+    end
+
+    def authorized
+      render json:{message: 'Please login'}, status: :unauthorized unless is_logged_in?
+    end
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,24 @@
 module Api::V1
   class UsersController < ApiController
+    before_action :authorized, except: [:sign_in, :create]
+
     def sign_in
       @user = User.find_by!(email:params[:email])
       if @user.valid_password?(params[:password])
         render json: { token: ApiAuthorization::TokenManager.encode_token({ user_id: @user.id} ) }
       else
-        render json: {}, status: :unauthorized
+        render json: {message: 'incorrect password'}, status: :unauthorized
       end
+    end
+
+    def create
+      @user = User.create!(user_registration_params)
+      render json: UserSerializer.new(@user) , status: :created
+    end
+
+    private
+    def user_registration_params
+      params.require(:user).permit(:email,:password)
     end
   end
 end
